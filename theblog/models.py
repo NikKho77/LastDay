0b8882458c6f55d3d6c0b9affd8c3602d75645e5
NikKho77 <68790627+NikKho77@users.noreply.github.com>
2020-09-14 09:52:28 +0300

    Update models.py

--- theblog/models.py
+++ theblog/models.py
@@ -87,7 +87,7 @@ class Contacts(models.Model):
     def __str__(self):
         return self.title
 
-class Home(models.Model):
+class Index(models.Model):
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
     text = models.TextField()
