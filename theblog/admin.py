cfaa6549b4931a6da3d01af5642442a2eac3a45d
NikKho77 <68790627+NikKho77@users.noreply.github.com>
2020-09-14 09:51:59 +0300

    Update admin.py

--- theblog/admin.py
+++ theblog/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Post,Onas,Gallery,Audio,Video,Contacts,Home,Repertuar,ManT,WomenT
+from .models import Post,Onas,Gallery,Audio,Video,Contacts,Index,Repertuar,ManT,WomenT
 
 admin.site.register(Post)
 admin.site.register(Onas)
@@ -7,7 +7,7 @@ admin.site.register(Gallery)
 admin.site.register(Audio)
 admin.site.register(Video)
 admin.site.register(Contacts)
-admin.site.register(Home)
+admin.site.register(Index)
 admin.site.register(Repertuar)
 admin.site.register(ManT)
 admin.site.register(WomenT)
